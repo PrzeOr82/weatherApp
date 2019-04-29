08ae3d7aa3e4db0b437c4a2fd831ce4a7f05a547
Przemek Orechwa <przemyslaw.orechwa@gmail.com>
2019-04-29 12:16:30 +0200

    JSON destructuring

--- js/main.js
+++ js/main.js
@@ -5,22 +5,14 @@ btn.addEventListener('click', function() {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&APPID=4fbcc4f736c166226d8d0b11a6fc32b8&units=metric`;
 
     let http = new XMLHttpRequest();
-    let main = {};
-    let clouds = {};
-    let sys = {};
-    let weather = {};
-    let wind = {};
 
     http.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(JSON.parse(http.responseText));
             let resp = JSON.parse(http.responseText);
 
-            main = resp.main;
-            clouds = resp.clouds;
-            sys = resp.sys;
-            weather = resp.weather;
-            wind = resp.wind;
+            // Object Destructuring
+            let { main, clouds, sys, weather, wind } = resp;
 
             document.querySelector('.city').innerHTML = resp.name;
             document.querySelector('.temp_actual').innerHTML = Math.floor(main.temp) + "°C";
